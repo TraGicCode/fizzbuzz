b3f7c36be88b52672cf7cf9a4214ae69056204fe
TraGicCode <6224270+TraGicCode@users.noreply.github.com>
2017-10-29 19:01:57 -0500

    Completed fizzbuzz in ruby

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,23 @@
 require "fizzbuzz/version"
 
 module Fizzbuzz
-  # Your code goes here...
+  def self.run
+    result = []
+    1.upto(100) do |number|
+      if self.check_multiple(number, 5) && self.check_multiple(number, 3)
+        result.push("fizzbuzz")
+      elsif self.check_multiple(number, 5)
+        result.push("buzz")
+      elsif self.check_multiple(number, 3)
+        result.push("fizz")
+      else
+        result.push(number)
+      end
+    end
+    result
+  end
+
+  def self.check_multiple(target, multiple)
+    (target % multiple) == 0
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,7 +5,48 @@ RSpec.describe Fizzbuzz do
     expect(Fizzbuzz::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe ".run" do
+    let(:result) { Fizzbuzz::run }
+    it "returns an array" do
+      expect(result).to be_a(Array)
+    end
+
+    it "returns a non-empty array" do
+      expect(result).to_not be_empty
+    end
+
+    it "returns an array of 100 items" do
+      expect(result.length).to eq(100)
+    end
+
+    it "should contain no multiples of 3" do
+      result.each do |number|
+        expect(number % 3).to_not eq(0)
+      end
+    end
+
+    [3, 6, 9].each do |number|
+      it "should return fizz for the #{number}nth element" do
+        expect(result[number-1]).to eq("fizz")
+      end
+    end
+
+    it "should contain no multiples of 5" do
+      result.each do |number|
+        expect(number % 5).to_not eq(0)
+      end
+    end
+
+    [5, 10, 20].each do |number|
+      it "should return buzz for the #{number}nth element" do
+        expect(result[number-1]).to eq("buzz")
+      end
+    end
+
+    [15, 30, 45].each do |number|
+      it "should return fizzbuzz for the #{number}nth element" do
+        expect(result[number-1]).to eq("fizzbuzz")
+      end
+    end
   end
 end
